9708bac0a87f46ea7c770d0d22175ca1ae60186d
A3Barbosa <a3barbosa21@gmail.com>
2023-05-16 14:26:00 -0400

    topmix-demo

---
+++ models/Post.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema({
+  drinkName: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    require: true,
+  },
+  cloudinaryId: {
+    type: String,
+    require: true,
+  },
+  caption: {
+    type: String,
+    required: true,
+  },
+
+  rating: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+ 
+});
+
+module.exports = mongoose.model("Post", PostSchema);
